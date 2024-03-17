3c59b192e0915bcf4756f1c4edbcf9f53b269b19
johnpaullazaro <jplazaro1295@gmail.com>
2024-03-17 15:19:05 +0800

    initStaticList

---
+++ BlazorWebApp/BlazorWebApp/Models/Tenant.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorWebApp.Models
+{
+    public class Tenant
+    {
+        public int TenantId { get; set; }
+
+        public string Name { get; set; }
+
+        public DateOnly StartDate { get; set; }
+
+        public DateOnly EndDate { get; set; }
+
+        public bool isActive { get; set; }
+
+    }
+}
